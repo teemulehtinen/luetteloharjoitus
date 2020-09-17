83312489b313b7cb3dea0062fe3d6d0ab7f8e011
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-09-17 15:35:24 +0300

    Add 3.7-3.8

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 let persons = require('./persons')
 
 const app = express()
 app.use(express.json())
+morgan.token('body', (req, res) => req.method == 'POST' ? JSON.stringify(req.body) : '')
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const bad_req = (res, message) => {
   res.status(400).json({error: message})
@@ -14,46 +17,45 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+  res.json(persons)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        console.log(person)
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
-    if (!req.body.name) {
-      return bad_req(res, 'name missing')
-    }
-    if (!req.body.number) {
-      return bad_req(res, 'number missing')
-    }
-    if (persons.find(p => p.name === req.body.name)) {
-      return bad_req(res, 'name must be unique')
-    }
-    const person = {
-      name: req.body.name,
-      number: req.body.number,
-      id: Math.floor(Math.random() * 1000000000)
-    }
-    persons = persons.concat(person)
-    res.json(person)
+  if (!req.body.name) {
+    return bad_req(res, 'name missing')
+  }
+  if (!req.body.number) {
+    return bad_req(res, 'number missing')
+  }
+  if (persons.find(p => p.name === req.body.name)) {
+    return bad_req(res, 'name must be unique')
+  }
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+    id: Math.floor(Math.random() * 1000000000)
+  }
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+  console.log(`Server running on ${PORT}`)
 })
