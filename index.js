2b7c5d759b9340047a616105c5abcf1ad51d6f42
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-09-17 20:42:06 +0300

    Add 3.15-3.18

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const Person = require('./models/person')
 
-// Configure web server
+// Configure web service and logging
 const app = express()
 app.use(express.static('build'))
 app.use(express.json())
@@ -15,64 +15,80 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 // Connect to db
 const dburi = process.env.MONGODB_URI
 console.log('connecting to', dburi)
-mongoose
-  .connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.set('useFindAndModify', false)
+mongoose.connect(dburi, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(result => console.log('connected to db'))
   .catch(error => console.log('error connecting to db', error.message))
 
-// Finally, create routes:
+// Create serviced routes:
 
-const bad_req = (res, message) => {
-  res.status(400).json({error: message})
-}
-
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
   //res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
   const now = new Date()
-  Person.count({}).then(count => {
-    res.send(`<p>Phonebook has info for ${count} people</p><p>${now}</p>`)
-  })
+  Person.countDocuments({})
+    .then(count => res.send(`<p>Phonebook has info for ${count} people</p><p>${now}</p>`))
+    .catch(err => next(err))
 })
 
-app.get('/api/persons', (req, res) => {  
+app.get('/api/persons', (req, res, next) => {  
   //res.json(persons)
-  Person.find({}).then(persons => {
-    res.json(persons)
-  })
+  Person.find({})
+    .then(persons => res.json(persons))
+    .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   //const id = Number(req.params.id)
   //const person = persons.find(p => p.id === id)
-  Person.findById(req.params.id).then(person => {
-    if (person) {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person === null) {
+        return res.status(404).end()
+      }
       res.json(person)
-    } else {
-      res.status(404).end()
-    }
-  })
+    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   //const id = Number(req.params.id)
   //persons = persons.filter(p => p.id !== id)
-  Person.findByIdAndDelete(req.params.id).then(() => {
-    res.status(204).end()
-  })
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(err => next(err))
 })
 
-app.post('/api/persons', (req, res) => {
+const requireNameAndNumber = (req, next) => {
   if (!req.body.name) {
-    return bad_req(res, 'name missing')
+    next({name: 'BadRequest', message: 'name missing'})
+    return false
   }
   if (!req.body.number) {
-    return bad_req(res, 'number missing')
+    next({name: 'BadRequest', message: 'number missing'})
+    return false
+  }
+  return true
+}
+
+app.put('/api/persons/:id', (req, res, next) => {
+  if (!requireNameAndNumber(req, next)) {
+    return false
+  }
+  Person.findByIdAndUpdate(req.params.id, {name: req.body.name, number: req.body.number}, {new: true})
+    .then(updated => res.json(updated))
+    .catch(err => next(err))
+})
+
+app.post('/api/persons', (req, res, next) => {
+  if (!requireNameAndNumber(req, next)) {
+    return false
   }
   //if (persons.find(p => p.name === req.body.name))
-  Person.find({name: req.body.name}).then(persons => {
-    if (persons.length > 0) {
-      bad_req(res, 'name must be unique')
-    } else {
+  Person.find({name: req.body.name})
+    .then(persons => {
+      if (persons.length > 0) {
+        return next({name: 'BadRequest', message: 'name must be unique'})
+      }
       const person = new Person({
         name: req.body.name,
         number: req.body.number
@@ -80,11 +96,31 @@ app.post('/api/persons', (req, res) => {
       })
       //persons = persons.concat(person)
       //res.json(person)
-      person.save().then(saved => res.json(saved))
-    }
-  })
+      person.save()
+        .then(saved => res.json(saved))
+        .catch(err => next(err))
+    })
+  .catch(err => next(err))
 })
 
+// Intercept bad routes
+app.use((req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+})
+
+// Custom error messages
+app.use((err, req, res, next) => {
+  console.error(err.message)
+  if (err.name === 'BadRequest') {
+    return res.status(400).json({ error: err.message })
+  }
+  if (err.name === 'CastError') {
+    return res.status(400).json({ error: 'malformatted id' })
+  }
+  next(err)
+})
+
+// Start serving
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
