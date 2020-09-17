0551f1b5bd8e8627f69fc2ffbd992c0fb6e6582d
lehtint6 <teemu.t.lehtinen@aalto.fi>
2020-09-17 15:19:26 +0300

    Add 3.2-3.6

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
+const { response } = require('express')
 const express = require('express')
-const persons = require('./persons')
+let persons = require('./persons')
 
 const app = express()
+app.use(express.json())
+
+const bad_req = (res, message) => {
+  res.status(400).json({error: message})
+}
+
+app.get('/info', (req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
 
 app.get('/api/persons', (req, res) => {
-   res.json(persons)
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        console.log(person)
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    if (!req.body.name) {
+      return bad_req(res, 'name missing')
+    }
+    if (!req.body.number) {
+      return bad_req(res, 'number missing')
+    }
+    if (persons.find(p => p.name === req.body.name)) {
+      return bad_req(res, 'name must be unique')
+    }
+    const person = {
+      name: req.body.name,
+      number: req.body.number,
+      id: Math.floor(Math.random() * 1000000000)
+    }
+    persons = persons.concat(person)
+    res.json(person)
 })
 
 const PORT = 3001
